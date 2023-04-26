e24fabf8f49953437bd260a6a51dd0024e47d778
Mehdi2707 <mehdi2707@live.fr>
2023-04-26 15:43:37 +0200

    formulaire administraton

--- src/Controller/Admin/RecipesController.php
+++ src/Controller/Admin/RecipesController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Recipes;
+use App\Form\RecipesFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/recettes', name: 'admin_recipes_')]
 class RecipesController extends AbstractController
@@ -19,20 +23,56 @@ class RecipesController extends AbstractController
     }
 
     #[Route('/ajout', name: 'add')]
-    public function add(): Response
+    public function add(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('admin/recipes/index.html.twig', [
-            'controller_name' => 'HomeController',
+
+        $recipe = new Recipes();
+
+        $form = $this->createForm(RecipesFormType::class, $recipe);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $slug = $slugger->slug($recipe->getTitle())->lower();
+            $recipe->setSlug($slug);
+
+            $entityManager->persist($recipe);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Recette ajouté avec succès');
+            return $this->redirectToRoute('admin_recipes_index');
+        }
+
+        return $this->render('admin/recipes/add.html.twig', [
+            'recipeForm' => $form->createView(),
         ]);
     }
 
     #[Route('/edition/{id}', name: 'edit')]
-    public function edit(Recipes $recipes): Response
+    public function edit(Recipes $recipes, Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $this->denyAccessUnlessGranted('RECIPE_EDIT', $recipes);
-        return $this->render('admin/recipes/index.html.twig', [
-            'controller_name' => 'HomeController',
+
+        $form = $this->createForm(RecipesFormType::class, $recipes);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $slug = $slugger->slug($recipes->getTitle())->lower();
+            $recipes->setSlug($slug);
+
+            $entityManager->persist($recipes);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Recette modifié avec succès');
+            return $this->redirectToRoute('admin_recipes_index');
+        }
+
+        return $this->render('admin/recipes/edit.html.twig', [
+            'recipeForm' => $form->createView(),
         ]);
     }
 
---
+++ src/Form/RecipesFormType.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Recipes;
+use App\Entity\Users;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecipesFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('preparationTime', NumberType::class, [
+                'label' => 'Temps de préparation',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('cookingTime', NumberType::class, [
+                'label' => 'Temps de cuisson',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('numberOfServings', NumberType::class, [
+                'label' => 'Nombre de portions',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('difficultyLevel', TextType::class, [
+                'label' => 'Niveau de difficulté',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('user', EntityType::class, [
+                'class' => Users::class,
+                'choice_label' => 'username',
+                'label' => 'Utilisateur'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Recipes::class,
+        ]);
+    }
+}
