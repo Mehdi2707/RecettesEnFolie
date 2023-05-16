9890d1b38348b3118f9f352967355cd6c99e111a
Mehdi2707 <mehdi2707@live.fr>
2023-05-16 13:52:31 +0200

    modification commentaire

--- public/assets/js/comments.js
+++ public/assets/js/comments.js
@@ -7,3 +7,43 @@ $(document).ready(function ()
         });
     });
 });
+
+$(document).on('click', '.edit-comment-btn', function(e) {
+    e.preventDefault();
+    const commentId = $(this).data('id');
+    const commentContent = $(this).siblings('span').text();
+    const urlRequest = $(this).data('request');
+
+    // Remplacez le contenu du commentaire par un formulaire d'édition
+    var formHtml = '<form class="edit-comment-form" action="' + urlRequest + '" method="post">' +
+    '<input type="hidden" name="comment_id" value="' + commentId + '">' +
+    '<textarea class="form-control" name="comment_content">' + commentContent + '</textarea>' +
+    '<button class="btn btn-sm" type="submit">Enregistrer</button>' +
+    '</form>';
+    $(this).siblings('span').replaceWith(formHtml);
+});
+
+// Gérez la soumission du formulaire d'édition avec AJAX
+$(document).on('submit', '.edit-comment-form', function(e) {
+    e.preventDefault();
+    var form = $(this);
+    var commentId = form.find('input[name="comment_id"]').val();
+    var commentContent = form.find('textarea[name="comment_content"]').val();
+
+    // Envoyez la requête AJAX pour mettre à jour le commentaire
+    $.ajax({
+        url: form.attr('action'),
+        type: 'POST',
+        data: {
+            comment_id: commentId,
+            comment_content: commentContent
+        },
+        success: function(response) {
+            // Mettez à jour le contenu du commentaire avec la nouvelle valeur
+            form.replaceWith('<span>' + commentContent + '</span>');
+        },
+        error: function(xhr, status, error) {
+            alert(xhr.responseJSON.message);
+        }
+    });
+});
--- src/Controller/RecipesController.php
+++ src/Controller/RecipesController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Comments;
 use App\Entity\Notes;
 use App\Form\CommentsFormType;
+use App\Repository\CommentsRepository;
 use App\Repository\NotesRepository;
 use App\Repository\RecipesRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -25,7 +26,7 @@ class RecipesController extends AbstractController
         ]);
     }
 
-    #[Route('/{slug}', name: 'details')]
+    #[Route('/recette/{slug}', name: 'details')]
     public function details($slug, RecipesRepository $recipesRepository, NotesRepository $notesRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
         $recipes = $recipesRepository->findOneBy(['slug' => $slug]);
@@ -74,9 +75,33 @@ class RecipesController extends AbstractController
             'recipe' => $recipes,
             'form' => $form->createView(),
             'note' => $noteUser,
-            'noteGeneral' => $moyenne
+            'noteGeneral' => $moyenne,
+            'user' => $user
         ]);
     }
+
+    #[Route('/modification-commentaire', name: 'edit_comment')]
+    public function editComment(Request $request, EntityManagerInterface $entityManager, CommentsRepository $commentsRepository): Response
+    {
+        $commentId = $request->request->get('comment_id');
+        $commentContent = $request->request->get('comment_content');
+        $user = $this->getUser();
+        $comment = $commentsRepository->findOneBy(['id' => $commentId, 'user' => $user]);
+
+        if (!$comment) {
+            return new JsonResponse(['message' => 'Erreur interne'], 500);
+        }
+
+        // Mettez à jour le contenu du commentaire
+        $comment->setContent($commentContent);
+        $comment->setUpdatedAt(new \DateTimeImmutable());
+
+        // Enregistrez les modifications dans la base de données
+        $entityManager->flush();
+
+        return new JsonResponse(['success' => true]);
+    }
+
 
     #[Route('/note/{slug}', name: 'stars_request')]
     public function starsRequest($slug, Request $request, RecipesRepository $recipesRepository, NotesRepository $notesRepository, EntityManagerInterface $entityManager)
